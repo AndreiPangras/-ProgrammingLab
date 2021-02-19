4573daf9dcf630b182cf197a1c98fceb37023519
Andrei Pangras <apangras@gmail.com>
2021-02-19 08:16:07 +0000

    cambio generale

---
+++ es lez 3/somma_shampoo.py
@@ -0,0 +1,13 @@
+#apro e leggo il file linea per linea 
+somma=0
+my_file = open('shampoo_sales.csv', 'r')
+for line in my_file:
+    #faccio lo split di ogni riga sulla virgola
+
+    elementi=line.split(",")
+    if(elementi[1]!='Sales\n'):
+        valore=float(elementi[1])
+        somma+=valore
+
+
+print(somma)
--- intro.py
+++ intro.py
@@ -1,8 +1,3 @@
-def lista(the_list):
-  somma=0
-  for item in the_list:
-    somma+=item
-  print("somma:".format(somma))
 
 
-lista_somma([1,4,10])
+
